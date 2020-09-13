586effca7e18649b96b8b447245c21a841304c8a
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-09-13 13:47:11 -0700

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,12 +9,19 @@ public func configure(_ app: Application) throws {
     let hostName = "localhost"
     let userName = "postgres"
     let dbName = "best_movies_db"
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? hostName,
-        username: Environment.get("DATABASE_USERNAME") ?? userName,
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? dbName
-    ), as: .psql)
+
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        app.databases.use(try .postgres(
+            url: databaseURL
+            ), as: .psql)
+    } else {
+        app.databases.use( .postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? hostName,
+            username: Environment.get("DATABASE_USERNAME") ?? userName,
+            password: Environment.get("DATABASE_PASSWORD") ?? "",
+            database: Environment.get("DATABASE_NAME") ?? dbName
+            ), as: .psql)
+    }
 
     app.migrations.add(CreateMovie())
 
